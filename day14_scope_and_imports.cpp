aac951b87a13132040ccecf80e5aa5d1d826438a
Brendan Creane <bcreane@riverbed.com>
2016-01-14 21:31:32 -0800

    provide O(n) solution too.

--- day14_scope_and_imports.cpp
+++ day14_scope_and_imports.cpp
@@ -5,7 +5,17 @@ Difference(const std::vector<int>& e) : elements(e)
 
 void computeDifference()
 {
+#ifdef LOG_N
     std::sort(elements.begin(), elements.end());
     maximumDifference = elements[elements.size()-1] - elements[0];
+#else
+    int min = INT_MIN, max = INT_MAX;
+    for (size_t ndx = 0; ndx < elements.size(); ++ndx) {
+	min = std::min(min, elements[ndx]);
+	max = std::max(max, elements[ndx]);
+    }
+
+    maximumDifference = max - min;
+#endif
 }
 
